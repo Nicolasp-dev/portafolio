e3370dcaa2c4a846f1e37ae2796826cc36a55190
nicolaspc-dev <nicolaspc.wd@gmail.com>
2022-08-18 20:42:40 -0500

    Side Icons Re-fixed

--- src/components/UI/SideIcons.js
+++ src/components/UI/SideIcons.js
@@ -5,9 +5,9 @@ const SideIcons = () => {
   return (
     <div className="hidden lg:flex fixed flex-col top-[18rem] left-0">
       <ul>
-        <li className="w-[11rem] h-[5rem] flex justify-between items-center ml-[-5.5rem] hover:ml-[-10px] duration-300 bg-[#0a66c2] rounded-tr-md text-[#fff]">
+        <li className="w-[10.5rem] h-[4.2rem] flex justify-between items-center ml-[-5.5rem] hover:ml-[-10px] duration-300 bg-[#0a66c2] rounded-tr-md text-[#fff]">
           <a
-            className="ml-2 flex justify-between items-center gap-8 text-gray-300 "
+            className="ml-2 flex justify-between items-center gap-7 text-gray-300 "
             href="https://www.linkedin.com/in/nicolaspe%C3%B1a-dev/"
             rel="noreferrer"
             target="_blank"
@@ -15,9 +15,9 @@ const SideIcons = () => {
             LinkedIn <FaLinkedin size={32} />
           </a>
         </li>
-        <li className="w-[11rem] h-[5rem] flex justify-between items-center ml-[-5.5rem] hover:ml-[-10px] duration-300 bg-[#1f1e1e] text-[#fff] rounded-br-md">
+        <li className="w-[10.5rem] h-[4.2rem] flex justify-between items-center ml-[-5.5rem] hover:ml-[-10px] duration-300 bg-[#1f1e1e] text-[#fff] rounded-br-md">
           <a
-            className="ml-2 flex justify-around items-center gap-8 text-gray-300 "
+            className="ml-2 flex justify-around items-center gap-7 text-gray-300 "
             href="https://github.com/Nicolas-pc-dev"
             rel="noreferrer"
             target="_blank"
